b7935d4357463afea3e884cd45e3da8b86ed892b
franco rosi <fran._bas@hotmail.com>
2018-03-22 22:39:35 -0300

    inicial

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string cadena;
+            string primero ="";
+            string segundo;
+            string operador="";
+            int total=0;
+            int auxiliar;
+            Console.WriteLine("ingrese una cuenta");
+            cadena = Console.ReadLine();
+
+            foreach (var item in cadena)
+            {
+                
+                if (int.TryParse(item.ToString(), out int numero))
+                {
+
+                    if (operador == ""){
+                        primero = primero + item.ToString();
+                    }
+                    
+                    
+                }
+                else
+                {
+                    operador = item.ToString();
+                   
+
+                }
+                
+            }
+            /*Console.WriteLine("ingrese su apellido");
+            apellido = Console.ReadLine();
+            */
+
+        }
+    }
+}
